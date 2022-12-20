caa84549e468514f362212aa65b5e7c79f309441
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2022-12-20 14:07:10 -0700

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoAPIOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d819be8354msha0e8511e72ae8efp1d3fdajsn152a3866597f",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const API_key = "c155af124e57b72687d829609c2b277a";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
